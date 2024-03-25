24173549d425e2fbacc618a60e0ec148e9454aca
Pucciland95 <niccolo.lucci@polimi.it>
2024-03-25 13:56:01 +0000

    Removed CIPster and Opener

--- CIPster/build/build-CIPster/src/eip-build/src/CMakeFiles/test_byte_bufs.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for test_byte_bufs.
-# This may be replaced when dependencies are built.
--- src/my_node.cpp
+++ src/my_node.cpp
@@ -1,10 +1,27 @@
-#include <cstdio>
+#include <iostream>
+#include <boost/asio.hpp>
 
-int main(int argc, char ** argv)
-{
-  (void) argc;
-  (void) argv;
+int main() {
 
-  printf("hello world my_package package\n");
-  return 0;
+    // Create io_context
+    boost::asio::io_context io_context;
+
+    // Create socket
+    boost::asio::ip::tcp::socket socket(io_context);
+
+    // Connect to robot gripper
+    socket.connect(boost::asio::ip::tcp::endpoint(boost::asio::ip::address::from_string("192.168.125.160"), 44818));
+
+    // Send data
+    // std::string data = "Hello, gripper!";
+    // socket.write_some(boost::asio::buffer(data));
+
+    // Receive data
+    char reply[1024];
+    std::cout << "Reply is aaaaa" << std::endl;
+
+    size_t reply_length = socket.read_some(boost::asio::buffer(reply, 10));
+    std::cout << "Reply is: " << std::string(reply, reply_length) << std::endl;
+
+    return 0;
 }
